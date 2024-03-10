bf72ee8dcb54d97f33e59197ffa88fb4fed50d51
Dev1012 <143276208+dev1012-lzh@users.noreply.github.com>
2024-03-10 18:56:31 +0800

    Version 0.1
    Provide Front-End User Touch - Link is not included

---
+++ giliranscript.js
@@ -0,0 +1,32 @@
+
+document.title = 'Semak Giliran';
+
+setInterval(() => {
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = '0' + (today.getMonth() + 1);
+    var day = today.getDate();
+
+    var hour = today.getHours();
+    if (hour > 12) {
+        var hour = hour - 12;
+        var daylight = 'pm';
+    }
+    var minute = today.getMinutes();
+    if (minute < 10) {
+        var minute = "0" + minute;
+    }
+    var second = today.getSeconds();
+    if (second < 10) {
+        var second = '0' + second;
+    }
+
+    var date = day + ' - ' + month + ' - ' + year;
+    var time = hour + ":" + minute + ":" + second;
+
+    var dateTime = date + ' ' + time;
+    console.log(dateTime)
+    document.querySelector('h3#time').innerHTML = date;
+    document.querySelector('h2#time').innerHTML = time + ' <span style="font-size:10px">' + daylight + '</span>';
+}, 1000);
+
