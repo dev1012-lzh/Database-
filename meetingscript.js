c5cb2323b3754cb7f9a85a4be44cc4990164afc5
Dev1012 <143276208+dev1012-lzh@users.noreply.github.com>
2024-03-21 18:52:52 +0800

    update

--- meetingscript.js
+++ meetingscript.js
@@ -35,7 +35,8 @@ setInterval(() => {
 function chkbox() {
     document.querySelector('button').innerHTML = 'Menyertai Temu Duga'
     document.querySelector('button').disabled = false;
-    document.querySelector('#chkbox').disabled = true;
+    document.querySelector('#chkbox').disabled = false;
+    alert('! 未到时间，请查看次序。')
     document.querySelector('button').style.cursor = 'pointer';
     document.querySelector('button').style.background = 'white';
 }
